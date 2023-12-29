e3c6d6362ca4db707f2f6299b92212698a8b1465
Leoscott <PRCSteven06@gmail.com>
2023-12-29 20:14:04 +0800

    新尝试

---
+++ backend/AppleDBv3/src/hello/helloworld.java
@@ -0,0 +1,22 @@
+package hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class helloworld extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // 设置响应类型:
+        resp.setContentType("text/html");
+        // 获取输出流:
+        PrintWriter pw = resp.getWriter();
+        // 写入响应:
+        pw.write("<h1>Hello, world!</h1>");
+        // 最后不要忘记flush强制输出:
+        pw.flush();
+    }
+}
