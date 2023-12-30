d59844f1ebbb79e62cfa2abe562c28210398afbc
Leoscott <PRCSteven06@gmail.com>
2023-12-30 14:56:15 +0800

    增加ui

---
+++ backend/managerLite/src/interf/userInterface.java
@@ -0,0 +1,12 @@
+package interf;
+
+public class userInterface {
+    public void welcome() {
+        String tableList = "warehouse admin raw product supplier account log";
+
+        System.out.println("------------欢迎------------");
+        System.out.println("直接输入名字即可查询");
+        System.out.println(tableList);
+        System.out.println("---------------------------");
+    }
+}
