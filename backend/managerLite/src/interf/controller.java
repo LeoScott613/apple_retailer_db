a9cfaa854659042995e03e2c58002e07ed601809
Leoscott <PRCSteven06@gmail.com>
2023-12-30 20:52:30 +0800

    更新界面，并将CRUD功能移动到switch语句中

--- backend/managerLite/src/interf/controller.java
+++ backend/managerLite/src/interf/controller.java
@@ -27,44 +27,53 @@ public class controller {
         String operation = in.nextLine();
 
         try {
+            // 将数据库连接和statement对象初始化好，以便实现CRUD的各个功能
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+
             switch (operation) {
                 case "select":
                 case "select ":
                     userInterface.selectHint();
                     tableName = in.nextLine();
                     midQuery = queryConstructor.selectQuery(tableName);
+
+                    //开始执行查询
+                    resultSet = statement.executeQuery(midQuery);
+                    metaData = resultSet.getMetaData();
+                    //1.获取表头
+                    System.out.println("Table: " + tableName);
+                    int columnCount = metaData.getColumnCount();
+                    for (int i = 1; i <= columnCount; i++) {
+//                System.out.println(metaData.getColumnLabel(i) + " (" + metaData.getColumnTypeName(i) + ")");
+                        System.out.print(metaData.getColumnLabel(i) + "\t\t");
+                    }
+                    System.out.println();
+                    //2.输出全表内容的通用方法：根据表格元数据进行循环输出，所有输出结果只是用Obj装载
+                    while (resultSet.next()) {
+                        // 通用输出模式
+                        for (int i = 1; i <= columnCount; i++) {
+                            // 获取不同类型的数据
+                            Object value = resultSet.getObject(i);
+                            System.out.print(value + "\t\t");
+                        }
+                        System.out.println();
+                    }
                     break;
                 case "insert":
+                    userInterface.insertHint();
+                    tableName = in.nextLine();
                     break;
                 default:
                     throw new SQLException();
             }
 
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, username, password);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(midQuery);
-            metaData = resultSet.getMetaData();
 
-            //获取表头
-            System.out.println("Table: " + tableName);
-            int columnCount = metaData.getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-//                System.out.println(metaData.getColumnLabel(i) + " (" + metaData.getColumnTypeName(i) + ")");
-                System.out.print(metaData.getColumnLabel(i) + "\t\t");
-            }
-            System.out.println();
 
-            // 查询的业务逻辑：根据表格元数据进行循环输出，所有输出结果只是用Obj装载
-            while (resultSet.next()) {
-                // 通用输出模式
-                for (int i = 1; i <= columnCount; i++) {
-                    // 获取不同类型的数据
-                    Object value = resultSet.getObject(i);
-                    System.out.print(value + "\t\t");
-                }
-                System.out.println();
-            }
+
+
+
         } catch (SQLException e) {
             System.out.println("SQL操作失败");
             e.printStackTrace();
--- backend/managerLite/src/interf/userInterface.java
+++ backend/managerLite/src/interf/userInterface.java
@@ -7,12 +7,23 @@ public class userInterface {
         System.out.println("------------欢迎------------");
         System.out.println("数据库中有的数据表");
         System.out.println(tableList);
-        System.out.println("\n支持的操作：select");
+        System.out.println("\n支持的操作：select, insert");
         System.out.println("---------------------------");
         System.out.println("请先输入操作");
     }
+
     public static void selectHint() {
-        System.out.println("输入你想要查询的表名，可以是表的中文名也可以是表的原名：");
+        /**
+         * 显示select功能的提示语
+         */
+        System.out.println("输入你想要查询的表名，表的中文名或者表的原名：");
+    }
+
+    public static void insertHint() {
+        /**
+         * 显示insert功能的提示语
+         */
+        System.out.println("输入你想插入的表名，表的中文名或者表的原名：");
     }
 
     public static void end() {
