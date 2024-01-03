c264f6e30ae23a9eac2664a194ffd3c12e634a3d
Leoscott <PRCSteven06@gmail.com>
2024-01-03 11:18:31 +0800

    更新功能已经可用

--- database_implementation/trigger.sql
+++ database_implementation/trigger.sql
@@ -9,11 +9,11 @@ begin
     set id=id+1;
 
     if (NEW.rinv>OLD.rinv) then
-        insert into log (lno,io,date,rno,rvar) values(id,'入',current_timestamp,new.rno-old.rno);
+        insert into log (lno,io,date,rno,rvar) values(id,'入',current_timestamp,NEW.rno,new.rno-old.rno);
     end if;
 
     if (NEW.rinv<OLD.rinv) then
-        insert into log (lno,io,date,rno,rvar) values(id,'出',current_timestamp,old.rno-new.rno);
+        insert into log (lno,io,date,rno,rvar) values(id,'出',current_timestamp,NEW.rno,old.rno-new.rno);
     end if;
 end;
 //
@@ -30,11 +30,11 @@ begin
     set id=id+1;
 
     if (NEW.pinv>OLD.pinv) then
-        insert into log (lno,io,date,pno,pvar) values(id,'入',current_timestamp,new.pno-old.pno);
+        insert into log (lno,io,date,pno,pvar) values(id,'入',current_timestamp,new.pno,new.pno-old.pno);
     end if;
 
     if (NEW.pinv<OLD.pinv) then
-        insert into log (lno,io,date,pno,pvar) values(id,'出',current_timestamp,old.pno-new.pno);
+        insert into log (lno,io,date,pno,pvar) values(id,'出',current_timestamp,new.pno,old.pno-new.pno);
     end if;
 end;
 //
