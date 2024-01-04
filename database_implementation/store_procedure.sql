653c21da79453872e23e2428e2b1714164313454
Leoscott <PRCSteven06@gmail.com>
2024-01-04 13:26:36 +0800

    添加新存储过程

--- database_implementation/store_procedure.sql
+++ database_implementation/store_procedure.sql
@@ -36,3 +36,29 @@ END //
 
 DELIMITER ;
 
+DELIMITER //
+
+CREATE PROCEDURE GetCapacity(IN warehouseNumber INT)
+BEGIN
+    DECLARE capacity INT;
+    DECLARE total INT;
+    DECLARE percentage INT;
+
+    SELECT rcap INTO capacity
+    FROM warehouse
+    WHERE wno = warehouseNumber;
+    SELECT cap INTO total
+    FROM warehouse
+    WHERE wno = warehouseNumber;
+
+    SET percentage = (capacity/total)*100;
+
+    IF capacity IS NOT NULL THEN
+        SELECT CONCAT('It', ' has ', percentage, ' % remaining.') AS Inventory;
+    ELSE
+        SELECT 'Warehouse not found' AS Inventory;
+    END IF;
+END //
+
+DELIMITER ;
+
